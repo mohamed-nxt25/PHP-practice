03a2235d6d2e9e1eca2f3a66da3b39b5e2e2b372
mohamed-nxt25 <maxamedadan2005@gmail.com>
2026-09-25 08:12:54 +0300

    WEEK 2 PRACTICE

---
+++ WEEK 2/ass1_multiply.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    // Assignment 1
+    for ($i=1; $i <= 3; $i++) { 
+        for ($j=1; $j <= 5; $j++) { 
+            echo "Row is $i, Column is $j, Result is " . $i * $j . "<br>";
+        }
+    }
+
+    ?>
+</body>
+</html>
